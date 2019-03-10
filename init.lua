691325053074d5ec0d230fe6cfbe2a5c2959a6b7
Serpent7776 <serpent7776@gmail.com>
2019-03-10 15:03:12 +0100

    preload music

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 local W, H
+local music
 
 local lf = {
 	_VERSION = '0.1.0',
@@ -6,13 +7,35 @@ local lf = {
 	viewport_height = 0,
 }
 
+local function preload_music(dir)
+	music = {}
+	local music_dir = dir .. '/music'
+	local files = love.filesystem.getDirectoryItems(music_dir)
+	for _, file in pairs(files) do
+		local path = music_dir .. '/' .. file
+		local m = love.audio.newSource(path, 'stream')
+		m:setLooping(true)
+		music[file] = m
+	end
+end
+
+local function preload_assets()
+	local dir = 'assets/preload'
+	preload_music(dir)
+end
+
 function lf.setup_viewport(width, height)
 	lf.viewport_width = width
 	lf.viewport_height = height
 end
 
+function lf.get_music(file_name)
+	return music[file_name]
+end
+
 function love.load()
 	W, H = love.graphics.getPixelDimensions()
+	preload_assets()
 	lf.init()
 end
 
