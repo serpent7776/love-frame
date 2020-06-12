646549d7cf5e2ad547529d99c351190ead5d9a1c
Serpent7776 <serpent7776@gmail.com>
2020-06-12 12:29:26 +0200

    added support for preferences
    for now there's music_vol and sound_vol

--- init.lua
+++ init.lua
@@ -7,10 +7,34 @@ local viewport_width
 local viewport_height
 local time_step
 local time_acc
+local prefs
+
+local max_vol = 100
 
 local lf = {
 	_VERSION = '0.1.0',
 }
+
+local function default_prefs()
+	return {
+		music_vol = max_vol,
+		sound_vol = max_vol,
+	}
+end
+
+local function read_prefs()
+	local fname = 'prefs'
+	if not love.filesystem.getInfo(fname) then
+		return false
+	end
+	for line in love.filesystem.lines(fname) do
+		local var, val = string.match(line, '^([%a_]*)%s*=%s*([%a%d]*)$')
+		if prefs[var] ~= nil then
+			prefs[var] = val
+		end
+	end
+	return true
+end
 
 local function preload_music(dir)
 	music = {}
@@ -20,6 +44,7 @@ local function preload_music(dir)
 		local path = music_dir .. '/' .. file
 		local m = love.audio.newSource(path, 'stream')
 		m:setLooping(true)
+		m:setVolume(prefs.music_vol / max_vol)
 		music[file] = m
 	end
 end
@@ -30,7 +55,9 @@ local function preload_sounds(dir)
 	local files = love.filesystem.getDirectoryItems(sounds_dir)
 	for _, file in pairs(files) do
 		local path = sounds_dir .. '/' .. file
-		sounds[file] = love.audio.newSource(path, 'static')
+		local s = love.audio.newSource(path, 'static')
+		s:setVolume(prefs.sound_vol / max_vol)
+		sounds[file] = s
 	end
 end
 
@@ -87,6 +114,8 @@ function love.load()
 	viewport_height = window_height
 	time_step = 1 / 50
 	time_acc = time_step
+	prefs = default_prefs()
+	read_prefs()
 	preload_assets()
 	lf.init()
 end
